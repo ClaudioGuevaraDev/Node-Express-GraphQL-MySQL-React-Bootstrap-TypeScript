2e57dcc909cfb2e91b96918308040a22accd66ae
ClaudioGuevaraDev <claudioguevaradev@gmail.com>
2022-03-14 00:18:53 -0300

    chau dist

--- server/dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("reflect-metadata");
-require("./database");
-const server_1 = require("./server");
-(0, server_1.startApolloServer)();
